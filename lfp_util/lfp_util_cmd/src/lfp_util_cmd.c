da9c3bc8f2f3ac6960da4a06321e19a4e6c118c4
Lee91 <13367267349@163.com>
2020-09-12 14:03:27 +0800

    readme跳转本地文件测试
    readme跳转本地文件测试

---
+++ lfp_util/lfp_util_cmd/src/lfp_util_cmd.c
@@ -0,0 +1,27 @@
+/***************************************************************************************
+File name: lfp_util_cmd.c
+Description: the cmd tools for debugging the program
+Author: wytaitaislee
+Version: V1.0.0
+Date: 2020-03-30
+History: 1. create file. -- 2020-03-30
+****************************************************************************************/
+
+#include "lfp_util.h"
+
+LFP_INT32 lfp_util_cmd_init(LFP_VOID)
+{
+    return LFP_OK;
+}
+
+/*@fn		  LFP_INT32 lfp_util_cmd_module_init(LFP_VOID)
+* @brief 	  loading command modules
+* @param[in]  LFP_VOID
+* @param[out] LFP_NULL
+* @return	  LFP_OK / LFP_ERR
+*/
+LFP_INT32 lfp_util_cmd_module_init(LFP_VOID)
+{
+    return LFP_OK;
+}
+
