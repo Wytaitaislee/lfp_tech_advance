19c4c1e770acc45da2718740d5d3ce3e8d982f07
Lee91 <13367267349@163.com>
2021-08-22 21:59:51 +0800

    线程池封装库相关代码

--- lfp_app_base/lfp_app_libs/lfp_libs_threadpool/src/lfp_app_threadpool.c
+++ lfp_app_base/lfp_app_libs/lfp_libs_threadpool/src/lfp_app_threadpool.c
@@ -4,5 +4,48 @@
  * @Author: wytaitaislee
  * @Date: 2021-03-21 17:59:18
  * @LastEditors: wytaitaislee
- * @LastEditTime: 2021-04-04 15:59:50
+ * @LastEditTime: 2021-08-22 19:04:04
  */
+
+#include "lfp_base.h"
+#include "lfp_libs_threadpool.h"
+
+WORK_QUEUE_T *work_queue_create(LFP_VOID)
+{
+    WORK_QUEUE_T *pWorkQueue = NULL;
+
+    pWorkQueue = (WORK_QUEUE_T*)LFP_MALLOC(sizeof(WORK_QUEUE_T)));
+    LFP_ASSERT_NULL_RET(pWorkQueue);
+    pWorkQueue->uiQueueCnt = 0;
+    pWorkQueue->pWorkList = LFP_NULL;
+    return pWorkQueue;
+}
+
+LFP_INT32 work_queue_add(WORK_QUEUE_T *pWorkQueue, WORK_QUEUE_T *pEntry)
+{
+    LFP_ASSERT_ERR_RET(pWorkQueue && pEntry);
+
+    lfp_dlist_add(pWorkQueue->, pEntry);
+}
+
+LFP_INT32 work_queue_pop()
+{
+
+}
+
+LFP_INT32 lfp_threadpool_create(LFP_UINT32 uiMaxThreads, LFP_UINT32 uiThreadTimeout, 
+                                LFP_VOID (*handler)(LFP_VOID*))
+{
+    
+}
+
+
+LFP_INT32 lfp_threadpool_dispatch(LFP_THREADPOOL_T *pThreadPool, LFP_VOID* pUsrData)
+{
+
+}
+
+LFP_INT32 lfp_threadpool_destory(LFP_THREADPOOL_T *pThreadPool)
+{
+
+}
