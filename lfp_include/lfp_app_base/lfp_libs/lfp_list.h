507ced7b70c347454b1e689e8dacff447cdd4d36
Wytaitaislee <13367267349@163.com>
2022-04-05 20:20:03 +0800

    revert

--- lfp_include/lfp_app_base/lfp_libs/lfp_list.h
+++ lfp_include/lfp_app_base/lfp_libs/lfp_list.h
@@ -33,9 +33,9 @@
     })
 
 /**
- * get the structure entity where the linked list element is located.
+ * get the structure entry where the linked list element is located.
  */
-#define LFP_LIST_ENTITY(ptr, type, member) LFP_CONTAINER_OF(ptr, type, member)
+#define LFP_LIST_ENTRY(ptr, type, member) LFP_CONTAINER_OF(ptr, type, member)
 
 /**
  * LFP_LIST_FOR_EACH	-	iterate over a list(from head)
@@ -56,33 +56,33 @@
          list_loop = (list_loop)->pPrev)
 
 /**
- * LFP_LIST_FIRST_ENTITY - get the first entity element from a list
+ * LFP_LIST_FIRST_ENTRY - get the first entry element from a list
  * @list_head:	the list head to take the element from.
  * @type:	    the type of the struct this is embedded in.
  * @member:	    the name of the "list_head" within the struct.
  * Note, that list is expected to be not empty.
  */
-#define LFP_LIST_FIRST_ENTITY(list_head, type, member) \
-    LFP_LIST_ENTITY((list_head)->pNext, type, member)
+#define LFP_LIST_FIRST_ENTRY(list_head, type, member) \
+    LFP_LIST_ENTRY((list_head)->pNext, type, member)
 
 /**
- * list_next_entity - get the next element in list
- * @entity:	the type * to cursor
+ * list_next_entry - get the next element in list
+ * @entry:	the type * to cursor
  * @member:	the name of the list_head within the struct.
  */
-#define LFP_LIST_NEXT_ENTITY(entity, member) \
-    LFP_LIST_ENTITY((entity)->member.pNext, typeof(*(entity)), member)
+#define LFP_LIST_NEXT_ENTRY(entry, member) \
+    LFP_LIST_ENTRY((entry)->member.pNext, typeof(*(entry)), member)
 
 /**
- * LFP_LIST_FOR_EACH_ENTITY	-	iterate over a entity(from the head
- * entity)
+ * LFP_LIST_FOR_EACH_ENTRY	-	iterate over a entry(from the head
+ * entry)
  * @list_loop:	the &struct "list_head" to use as a loop cursor.
  * @head:	    the head for your list.
  */
-#define LFP_LIST_FOR_EACH_ENTITY(entity_loop, list_head, member)               \
-    for (entity_loop =                                                         \
-             LFP_LIST_FIRST_ENTITY(list_head, typeof(*(entity_loop)), member); \
-         &entity_loop->member != (list_head);                                  \
-         entity_loop = LFP_LIST_NEXT_ENTITY(entity_loop, member))
+#define LFP_LIST_FOR_EACH_ENTRY(entry_loop, list_head, member)               \
+    for (entry_loop =                                                        \
+             LFP_LIST_FIRST_ENTRY(list_head, typeof(*(entry_loop)), member); \
+         &entry_loop->member != (list_head);                                 \
+         entry_loop = LFP_LIST_NEXT_ENTRY(entry_loop, member))
 
 #endif /*__LFP_LIBS_LIST_H__*/
