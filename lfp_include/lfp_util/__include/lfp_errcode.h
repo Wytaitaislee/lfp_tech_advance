2ac5e0f7d2ae6a00e9289a93114c92845ff8d0f7
Wytaitaislee <13367267349@163.com>
2022-04-10 15:54:16 +0800

    增加全局错误码定义规则

---
+++ lfp_include/lfp_util/__include/lfp_errcode.h
@@ -0,0 +1,14 @@
+/***
+ * @FilePath     : lfp_errcode.h
+ * @Description  : global errcode rules.
+ * @Author       : wytaitaislee
+ * @Date         : 2022-04-10 15:04:16
+ * @LastEditTime : 2022-04-10 15:09:22
+ * @LastEditors  : wytaitaislee
+ * @Copyright 2022 wytaitaislee, All Rights Reserved.
+ */
+
+/*+-------+---------------+------------------+-----------------------+-------------------------+
+|  Bit  |31|30|29|28|27|26|25|24|23|22|21|20|19|18|17|16|15|14|13|12|11|10|9|8|7|6|5|4|3|2|1|0|
+| Field |            category               |        module         |         mask            |
+*/
